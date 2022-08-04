97d8d21c7a45629c60aa6300b10231d94e60476b
shohamD1121 <shohamdouek@gmail.com>
2022-08-05 01:58:46 +0300

    Added Contact Component Functionality

--- src/components/ContactUs/ContactForm.tsx
+++ src/components/ContactUs/ContactForm.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 type Props = {};
 
 const ContactForm = (props: Props) => {
+  const form: any = useRef();
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("gmail", "template_xwpiymb", form.current, "i4Ma9iw8nDyzcYwQ0")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    form.current.reset();
+  };
   return (
     <div className="flex mt-5 justify-center items-center flex-1 w-full flex-col py-16 px-8">
-      <h1 className="md:text-5xl text-3xl font-bold md:mb-20 mb-6 2xl:text-6xl leading-8">Contact Us</h1>
-      <form className="w-full max-w-lg">
+      <h1 className="md:text-5xl text-3xl font-bold md:mb-20 mb-6 2xl:text-6xl leading-8">
+        Contact Us
+      </h1>
+      <form ref={form} className="w-full max-w-lg" onSubmit={sendEmail}>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              First Name
+              Name
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
               type="text"
-              placeholder=""
+              name="name"
             />
             {/* <p className="text-red-500 text-xs italic">
               Please fill out this field. 
@@ -25,25 +43,24 @@ const ContactForm = (props: Props) => {
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Last Name
+              Email
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
-              type="text"
-              placeholder=""
+              type="email"
+              name="email"
             />
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              E-mail
+              Subject
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="email"
-              type="email"
+              type="text"
+              name="subject"
             />
           </div>
         </div>
@@ -52,17 +69,18 @@ const ContactForm = (props: Props) => {
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Message
             </label>
-            <textarea
-              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-              id="message"
-            ></textarea>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              name="message"
+            />
           </div>
         </div>
         <div className="md:flex md:items-center">
           <div className="md:w-1/3 w-full">
             <button
               className="shadow bg-green hover:bg-blue focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded md:w-auto w-full"
-              type="button"
+              type="submit"
             >
               Send
             </button>
