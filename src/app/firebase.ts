633fdd0fdcda0d85ffa0d14f0f8dff110609be36
shohamD1121 <shohamdouek@gmail.com>
2022-08-05 22:50:55 +0300

    dumb changes

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/database"; // If using Firebase database
 
 const firebaseConfig = {
-  apiKey: process.env.Apy_Key,
-  authDomain: process.env.Auth_Domain,
-  databaseURL: process.env.Database_URL,
-  projectId: process.env.Project_Id,
-  storageBucket: process.env.Storage_Bucket,
-  messagingSenderId: process.env.Messaging_Sender_Id,
-  appId: process.env.App_Id,
-  measurementId: process.env.Measurement_Id,
+  apiKey: "AIzaSyDxFJ29RaoGA4yo4HGO2p0u34Jg4Gx_3OY",
+  authDomain: "stocker-website-emails.firebaseapp.com",
+  databaseURL: "https://stocker-website-emails-default-rtdb.firebaseio.com",
+  projectId: "stocker-website-emails",
+  storageBucket: "stocker-website-emails.appspot.com",
+  messagingSenderId: "124400140284",
+  appId: "1:124400140284:web:e177b499ab1ff1757f3c79",
+  measurementId: "G-6DRCKYFRQS",
 };
 
 firebase.initializeApp(firebaseConfig);
